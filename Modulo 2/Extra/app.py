4ee01d01d98371a233efd81952986dcade124162
guilhermealves001 <guigofort@gmail.com>
2025-10-24 16:38:38 -0300

    Add files via upload

---
+++ Modulo 2/Extra/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+# cria uma instância de um servidor flask
+app = Flask(__name__)
+# cria rota para a página principal do site 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/estoque')
+def estoque():
+    return render_template('estoque.html')
+# executa o servidor flask
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# no terminal, executar o arquivo app.py
+
+#python app.py
+
+# o servidor flask vai ser executado e rodar no endereço
+# 127.0.0.1:5000
